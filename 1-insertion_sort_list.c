1020f38593ba2a2ebab1a417a186827e8b7f35e5
SilverDontAsk <steven_rosario@penguin>
2024-04-12 12:25:44 -0400

    I am not a computer scientist

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,43 +1,46 @@
 #include "sort.h"
-#include <stdio.h>
 /**
- * insertion_sort_list - sorts in insertion sort algorithm
- * @list: pointer to pointer to list
- * Return: sorted doubly linked list
+ * swap_nodes - helper function to swap two nodes in a listint_t
+ * doubly linked list.
+ * @h: pointer to the head of the doubly-linked list
+ * @n1: pointer to the first node to swap
+ * @n2: second node to swap
+ * Return: nothing
+ */
+void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2)
+{
+(*n1)->next = n2->next;
+if (n2->next != NULL)
+n2->next->prev = *n1;
+n2->prev = (*n1)->prev;
+n2->next = *n1;
+if ((*n1)->prev != NULL)
+(*n1)->prev->next = n2;
+else
+*h = n2;
+(*n1)->prev = n2;
+*n1 = n2->prev;
+}
+/**
+ * insertion_sort_list - insertion sorting algorithm for doubly linked
+ * listint_t lists.
+ * @list: pointer to pointer to head of list
+ * Return: nothing
  */
 void insertion_sort_list(listint_t **list)
 {
-listint_t *sorted = NULL;
-listint_t *current;
-listint_t *next;
-listint_t *ptr;
 if (list == NULL || *list == NULL || (*list)->next == NULL)
 return;
-current = *list;
+listint_t *current = (*list)->next;
 while (current != NULL)
 {
-next = current->next;
-if (sorted == NULL || current->n <= sorted->n)
+listint_t *insert = current->prev;
+while (insert != NULL && current->n < insert->n)
 {
-current->prev = NULL;
-current->next = sorted;
-if (sorted != NULL)
-sorted->prev = current;
-sorted = current;
+swap_nodes(list, &insert, current);
+print_list((const listint_t *)*list);
+insert = current->prev;
 }
-else
-{
-ptr = sorted;
-while (ptr->next != NULL && ptr->next->n < current->n)
-ptr = ptr->next;
-current->prev = ptr;
-current->next = ptr->next;
-if (ptr->next != NULL)
-ptr->next->prev = current;
-ptr->next = current;
+current = current->next;
 }
-current = next;
-print_list(sorted);
-}
-*list = sorted;
 }
